05a38984d4eee263fd181acc37ad0cae8b3dd895
qiuwei <Wcbc0310@126.com>
2019-12-06 15:52:29 +0800

    添加hystrix依赖  开启熔断配置

--- cloud-consumer/src/main/java/com/cloud/consumer/ConsumerController.java
+++ cloud-consumer/src/main/java/com/cloud/consumer/ConsumerController.java
@@ -1,5 +1,8 @@
 package com.cloud.consumer;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
+import com.sun.tools.internal.ws.wsdl.document.soap.SOAPUse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +14,9 @@ import org.springframework.web.client.RestTemplate;
  */
 @RestController
 public class ConsumerController {
+
+    private static int NUM = 1;
+
     @Autowired
     ConsumerService consumerService;
 
@@ -24,11 +30,39 @@ public class ConsumerController {
     }
 
 
-
-
     @GetMapping("ribbon")
-    public String ribbon() {
-        return restTemplate.getForObject("http://eureka-client/ribbon", String.class);
+    @HystrixCommand(fallbackMethod = "getRibbonDefaultInfo")
+    public String ribbon() throws InterruptedException {
+
+        String result = "";
+
+        if (NUM % 2 == 0) {
+
+            NUM += 1;
+            System.out.println("线程模拟熔断 睡眠3秒");
+            Thread.sleep(3000);
+        } else {
+            NUM += 1;
+            result = restTemplate.getForObject("http://eureka-client/ribbon", String.class);
+
+        }
+
+
+        System.out.println("当前请求次数 NUM:" + NUM);
+
+        return result;
+
     }
+
+
+    /**
+     * 熔断方法
+     * ribbon
+     */
+
+    public String getRibbonDefaultInfo() {
+        return "ribbon 类型 服务熔断";
+    }
+
 
 }
