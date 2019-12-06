8418ed117a6325bd2a9364ad8db3dc30935295ba
qiuwei <Wcbc0310@126.com>
2019-12-06 14:57:12 +0800

    修改启动类名称 , 添加maven启动制定启动类

--- cloud-provide-bak/src/main/java/com/cloud/client/ProviderBakApplication.java
+++ cloud-provide-bak/src/main/java/com/cloud/client/ProviderBakApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ClientApplication {
+public class ProviderBakApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
+        SpringApplication.run(ProviderBakApplication.class, args);
     }
 
 }
