a737e90b39b09046ee9a4b53208513aca0aceb63
qiuwei <Wcbc0310@126.com>
2020-08-30 10:26:50 +0800

    fix

--- eureka-server/src/main/java/com/cloud/eureka/WebSecurityConfig.java
+++ eureka-server/src/main/java/com/cloud/eureka/WebSecurityConfig.java
@@ -16,6 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable(); // 关闭csrf
+        http.csrf().ignoringAntMatchers("/eureka/**");//让csrf忽略该路径的请求
         http.authorizeRequests().anyRequest().authenticated().and().httpBasic(); // 开启认证
     }
 }
